b77751fe2b83342c3be794ba6ae065cac6f9adac
worasit <worasit.daimongkol@thoughtworks.com>
2022-09-11 14:52:05 +0700

    Created animal and breed dropdown.

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle WA");
-  console.log(location);
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
   return (
     <div className="search-params">
       <form>
@@ -14,6 +18,45 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(event) => setLocation(event.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(event) => {
+              setAnimal(event.target.value);
+              setBreed("");
+            }}
+            onBlur={(event) => {
+              setAnimal(event.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} id={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            disabled={!breeds.length}
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
