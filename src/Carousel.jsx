f6842cd52201c0a1e95b2e51699173aba5fc05c1
worasit <worasit.daimongkol@thoughtworks.com>
2022-09-25 18:29:53 +0700

    Applied ability to select image to Carousel.jsx

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 
 const Carousel = ({ images }) => {
-  const [active] = useState(0);
+  const [active, setActive] = useState(0);
 
   if (!images) {
     images = ["http://pets-images.dev-apis.com/pets/none.jpg"];
   }
+
+  const handleIndexClick = (event) => {
+    setActive(parseInt(event.target.dataset.index));
+  };
 
   return (
     <div className="carousel">
@@ -13,11 +17,14 @@ const Carousel = ({ images }) => {
       <div className="carousel-smaller">
         {images.map((photo, index) => {
           return (
+            // eslint-disable-next-line jsx-a11y/click-events-have-key-events
             <img
               src={photo}
               key={photo}
               className={index === active ? "active" : ""}
+              data-index={index}
               alt="animal thumbnail"
+              onClick={handleIndexClick}
             />
           );
         })}
